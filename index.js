97828e6c969e00015a20cd3027cb4d9028b437f2
Brody <brodywilton@hotmail.com>
2019-01-24 23:45:50 +0000

    Correct sound plays when user clicks a colourgit add index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var buttonColours = ['red','blue','green','yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
 
+//click handler for coloured panels
+$('.btn').click(function(event){
+  //store the id of the clicked square
+  var userChosenColour = $(this).attr('id');
+  //add the chosen square to the player pattern
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
+
 function nextSequence(){
   //create a random number between 0 and 3
   var randomNumber = Math.floor(Math.random() * 4);
@@ -13,22 +22,13 @@ function nextSequence(){
   //choose the colour chosen by randomChosenColour
   $('#' + randomChosenColour).fadeOut(100).fadeIn(100);
 
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
   //create a new audio sound based on chosen colour
-  var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
-  //play audio
+  var audio = new Audio('sounds/' + name + '.mp3');
   audio.play();
 }
 
 nextSequence();
-//user must press a key to start the game
-// $(document).on('keypress', nextSequence);
-
-//click handler for coloured panels
-$('.btn').click(function(event){
-  //store the id of the clicked square
-  var userChosenColour = $(event.target).attr('id');
-  //add the chosen square to the player pattern
-  userClickedPattern.push(userChosenColour);
-
-  console.log(userClickedPattern);
-});
